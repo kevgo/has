aba19fa0b0864caccd72555f736b6b164f962a80
Kevin Goslar <kevin.goslar@gmail.com>
2022-10-07 13:23:08 -0500

    Ignore ANSI formatting codes

--- src/checks/empty_output.rs
+++ src/checks/empty_output.rs
@@ -7,6 +7,7 @@ pub fn empty_output(cmd: String, args: Vec<String>) -> Result<bool, UserError> {
         .args(args)
         .output()
         .map_err(|_| UserError::UnknownCommand(cmd))?;
-    let text = str::from_utf8(&output.stdout).map_err(|_| UserError::NonUnicodeAppOutput)?;
+    let stripped = strip_ansi_escapes::strip(&output.stdout).expect("cannot strip ansi codes");
+    let text = str::from_utf8(&stripped).map_err(|_| UserError::NonUnicodeAppOutput)?;
     Ok(text.trim().is_empty())
 }
