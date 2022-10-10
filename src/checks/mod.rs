a6fd9fab9048abaa1bfdea2eb9acf757a034fd01
Kevin Goslar <kevin.goslar@gmail.com>
2022-10-10 18:23:05 -0500

    unpushed-commits.feature mod.rs unpushed_commits.rs main.rs

--- src/checks/mod.rs
+++ src/checks/mod.rs
@@ -3,9 +3,9 @@ pub mod file;
 mod folder;
 pub mod git_branch;
 mod uncommitted_changes;
-mod unpushed_changes;
+mod unpushed_commits;
 
 pub use empty_output::empty_output;
 pub use folder::folder;
 pub use uncommitted_changes::uncommitted_changes;
-pub use unpushed_changes::unpushed_changes;
+pub use unpushed_commits::unpushed_commits;
