c7b57c793e65419ee4843f4d2907bae22697061b
Kevin Goslar <kevin.goslar@gmail.com>
2022-10-10 10:33:21 -0500

    git_branch.rs

--- src/checks/git_branch.rs
+++ src/checks/git_branch.rs
@@ -6,10 +6,12 @@ pub fn local(name: &str) -> bool {
     git::has_branch(name)
 }
 
+/// indicates whether the local Git workplace has the given branch checked out
 pub fn local_active(name: &str) -> Result<bool, UserError> {
     Ok(git::current_branch()? == name)
 }
 
+/// indicates whether the local Git workplace has the given branch but not checked out
 pub fn local_inactive(name: &str) -> Result<bool, UserError> {
     Ok(local(name) && !local_active(name)?)
 }
