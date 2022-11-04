02a3414d0a43bc72f67fbad5febeb8609c25828c
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-04 16:59:14 -0500

    Big S

--- src/git/current_branch.rs
+++ src/git/current_branch.rs
@@ -1,11 +1,12 @@
 use crate::errors::UserError;
+use big_s::S;
 use std::process::Command;
 
 pub fn current_branch() -> Result<String, UserError> {
     let output = Command::new("git")
         .args(vec!["rev-parse", "--abbrev-ref", "HEAD"])
         .output()
-        .map_err(|_| UserError::UnknownCommand("git".into()))?;
+        .map_err(|_| UserError::UnknownCommand(S("git")))?;
     match String::from_utf8(output.stdout) {
         Ok(stdout) => Ok(stdout.trim().to_string()),
         Err(_) => Err(UserError::GitBranchNameInvalidUnicode),
