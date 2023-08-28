b46668d61c573f6170714eebbe17cb238ab0a439
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-28 15:55:17 -0500

    has_author.rs mod.rs

--- src/git/has_author.rs
+++ src/git/has_author.rs
@@ -1,12 +1,17 @@
+use crate::errors::GIT_NOT_INSTALLED;
 use std::process::Command;
 
 pub fn has_author(name: &str) -> bool {
     let output = Command::new("git")
-        .args(vec!["log", "--pretty=format:\"%an %ae\""])
+        .args(vec![
+            "log",
+            &format!("--author={name}"),
+            "--pretty=format:%h",
+        ])
         .output()
-        .expect();
-    if output.status.success()
-
-    // .status
-    // .success();
+        .expect(GIT_NOT_INSTALLED);
+    if !output.status.success() {
+        return false;
+    }
+    output.stdout.len() > 0
 }
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -4,5 +4,6 @@ mod has_branch;
 mod has_unpushed_commits;
 
 pub(crate) use current_branch::current_branch;
+pub(crate) use has_author::has_author;
 pub(crate) use has_branch::has_branch;
 pub(crate) use has_unpushed_commits::has_unpushed_commits;
