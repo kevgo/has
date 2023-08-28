41aaa3846b5fdeb9fae242e6f705934218cc6726
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-28 15:39:09 -0500

    Consistent error handling (#45)

--- src/git/current_branch.rs
+++ src/git/current_branch.rs
@@ -1,12 +1,11 @@
-use crate::errors::UserError;
-use big_s::S;
+use crate::errors::{UserError, GIT_NOT_INSTALLED};
 use std::process::Command;
 
 pub fn current_branch() -> Result<String, UserError> {
     let output = Command::new("git")
         .args(vec!["rev-parse", "--abbrev-ref", "HEAD"])
         .output()
-        .map_err(|_| UserError::UnknownCommand(S("git")))?;
+        .expect(GIT_NOT_INSTALLED);
     match String::from_utf8(output.stdout) {
         Ok(stdout) => Ok(stdout.trim().to_string()),
         Err(_) => Err(UserError::GitBranchNameInvalidUnicode),
