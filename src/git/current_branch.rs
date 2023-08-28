daa18e8f0c6966cd2702640aac4908cb6e095a42
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-28 15:29:03 -0500

    current_branch.rs

--- src/git/current_branch.rs
+++ src/git/current_branch.rs
@@ -1,12 +1,11 @@
 use crate::errors::UserError;
-use big_s::S;
 use std::process::Command;
 
 pub fn current_branch() -> Result<String, UserError> {
     let output = Command::new("git")
         .args(vec!["rev-parse", "--abbrev-ref", "HEAD"])
         .output()
-        .map_err(|_| UserError::UnknownCommand(S("git")))?;
+        .expect("git not installed");
     match String::from_utf8(output.stdout) {
         Ok(stdout) => Ok(stdout.trim().to_string()),
         Err(_) => Err(UserError::GitBranchNameInvalidUnicode),
