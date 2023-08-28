149365bb1b8da24352c8e2bf7c1247eda1aa3aca
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-28 15:36:30 -0500

    current_branch.rs has_branch.rs has_unpushed_commits.rs

--- src/git/current_branch.rs
+++ src/git/current_branch.rs
@@ -1,11 +1,11 @@
-use crate::errors::{self, UserError};
+use crate::errors::{UserError, GIT_NOT_INSTALLED};
 use std::process::Command;
 
 pub fn current_branch() -> Result<String, UserError> {
     let output = Command::new("git")
         .args(vec!["rev-parse", "--abbrev-ref", "HEAD"])
         .output()
-        .expect(errors::GIT_NOT_INSTALLED);
+        .expect(GIT_NOT_INSTALLED);
     match String::from_utf8(output.stdout) {
         Ok(stdout) => Ok(stdout.trim().to_string()),
         Err(_) => Err(UserError::GitBranchNameInvalidUnicode),
--- src/git/has_branch.rs
+++ src/git/has_branch.rs
@@ -1,11 +1,11 @@
-use crate::errors;
+use crate::errors::GIT_NOT_INSTALLED;
 use std::process::Command;
 
 pub fn has_branch(name: &str) -> bool {
     Command::new("git")
         .args(vec!["show-ref", "--verify", &format!("refs/heads/{name}")])
         .output()
-        .expect(errors::GIT_NOT_INSTALLED)
+        .expect(GIT_NOT_INSTALLED)
         .status
         .success()
 }
